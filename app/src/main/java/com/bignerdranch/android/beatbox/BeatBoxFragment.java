445f906cedef8d57a62aac56d20b5dd254bdbf55
Daniel Makover <dmakover@gmail.com>
2018-05-16 06:37:19 -0400

    init 2

--- app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/bignerdranch/android/beatbox/BeatBoxFragment.java
@@ -44,11 +44,15 @@ public class BeatBoxFragment extends Fragment {
         private SoundHolder(ListItemSoundBinding binding){
             super(binding.getRoot());
             mBinding = binding;
-            mBinding.setViewModel(new SoundViewModel(mBeatBox));
+            //NOTE: problems with databinging. Cannot implement setViewModel() on mBidining.
+            ///Maybe problem with dependecies?
+            
+            //mBinding.setViewModel(new SoundViewModel(mBeatBox));
         }
 
         public void bind(Sound sound){
-            mBinding.getViewModel().getSound(sound);
+            //NOTE: problems with databinging. Cannot implement getViewModel() on mBidining.
+            //mBinding.getViewModel().getSound(sound);
             mBinding.executePendingBindings();
         }
     }
